98998bb9594dbb4bcfa0d988df3864d4b58952a1
sam <samleefretwell@gmail.com>
2021-09-24 11:06:30 +0100

    Initial commit

---
+++ lib/kold_ruby.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kold_ruby/version"
+
+module KoldRuby
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/kold_ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe KoldRuby do
+  it "has a version number" do
+    expect(KoldRuby::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
